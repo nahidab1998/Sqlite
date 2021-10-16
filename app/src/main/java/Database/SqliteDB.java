65d00b76ba2dc7ac45ca87a7a3f2fe37e97b3d45
nahid.ab <n.alibagheri1998@gmail.com>
2021-10-16 16:13:14 +0330

    complementary process 1

--- app/src/main/java/Database/SqliteDB.java
+++ app/src/main/java/Database/SqliteDB.java
@@ -1,2 +1,42 @@
-package Database;public class SqliteDB {
+package Database;
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SqliteDB extends SQLiteOpenHelper {
+
+    public static final String DB_name="my_sqlite";
+    public static final int DB_version=1;
+
+
+    public static final String table_user="my_user";
+    public static final String ID="id";
+    public static final String NAME="name";
+    public static final String MOBILE="mobile";
+    public static final String PICTURE="picture";
+
+    public SqliteDB(Activity context) {
+        super(context,DB_name,null,DB_version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query="CREATE TABLE IF NOT EXISTS "+ table_user +" ( " +
+                ID + " INTEGER PRIMARY KEY AUTOINCREMENT , " +
+                NAME + " TEXT NOT NULL , " +
+                MOBILE + " TEXT )";
+        //ADD IMG
+
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query= "DROP TABLE IF EXISTS " + table_user;
+        db.execSQL(query);
+
+        onCreate(db);
+
+    }
 }
--- app/src/main/java/Database/UserDataAccess.java
+++ app/src/main/java/Database/UserDataAccess.java
@@ -1,4 +1,73 @@
 package Database;
 
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import java.util.ArrayList;
+import java.util.List;
+import model.UserModel;
+
 public class UserDataAccess {
+
+    private SQLiteOpenHelper openHelper;
+    private SQLiteDatabase database;
+    private String[] cls ={SqliteDB.ID , SqliteDB.NAME , SqliteDB.MOBILE};
+
+
+    public UserDataAccess(Activity context) {
+        openHelper=new SqliteDB(context);
+    }
+
+    public void openDB(){
+
+        database=openHelper.getWritableDatabase();
+
+    }
+
+    public void closeDB(){
+
+        database.close();
+    }
+
+    public boolean addNewUser(UserModel model){
+        try {
+
+            String name = model.getName();
+            String mobile =model.getMobile();
+            String addQuery ="insert into " + SqliteDB.table_user + " (" + SqliteDB.NAME + " , " + SqliteDB.MOBILE + " , " + ")" +
+                    "values (" + "'" + name + "'" + " , " + "'" + mobile + "'" + " , ";
+
+            database.execSQL(addQuery);
+
+            return true;
+
+        }catch (Exception e){
+
+            return false;
+        }
+    }
+    public List<UserModel> getallusers(){
+
+        List<UserModel> models =new ArrayList<>();
+
+        String getallquery = "select * from " + SqliteDB.table_user;
+
+        Cursor cursor = database.rawQuery(getallquery, null);
+
+        if (cursor.moveToFirst()){
+            do {
+
+                int id = cursor.getInt(cursor.getColumnIndex(SqliteDB.ID));
+                String name = cursor.getString(cursor.getColumnIndex(SqliteDB.NAME));
+                String mobile = cursor.getString(cursor.getColumnIndex(SqliteDB.MOBILE));
+
+                models.add(new UserModel(id , name , mobile));
+
+            }while (cursor.moveToNext());
+
+        }
+
+        return models;
+    }
 }
